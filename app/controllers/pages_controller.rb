18d67012ec2b6f5d0830b0727a3ec56f5844d051
Adrien Loiseau <adrien.lois@gmail.com>
2020-08-19 15:09:05 +0200

    Created search-bar for home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
   def home
+    @celebrities = Celebrity.all
   end
 end
--- app/models/celebrity.rb
+++ app/models/celebrity.rb
@@ -3,4 +3,7 @@ class Celebrity < ApplicationRecord
   has_many :bookings
 
   has_one_attached :photo
+
+  CATEGORIES = ["sport", "music", "history", "politic", "art", "technology"]
+  validates :category, inclusion: { in: CATEGORIES }
 end
