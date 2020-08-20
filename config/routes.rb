d6719bb9fcdc96bbc54a176727585186779fe75e
Lea Bugeat <lbugeat@gmail.com>
2020-08-20 17:27:36 +0200

    add back on recap resa

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :celebrities, only: [:index, :show] do
-    resources :bookings, only: [:index, :new, :create]
+    resources :bookings, only: [:show, :new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
